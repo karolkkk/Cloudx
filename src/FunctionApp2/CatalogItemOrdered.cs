1976b61631a123036ccbdd868f6e5df19ff70694
Karolina Krauze <karolina_krauze@epam.com>
2021-11-24 10:05:25 +0100

    add db

---
+++ src/FunctionApp2/CatalogItemOrdered.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp2
+{
+    public class CatalogItemOrdered // ValueObject
+    {
+        public CatalogItemOrdered(int catalogItemId, string productName, string pictureUri)
+        {
+           
+
+            CatalogItemId = catalogItemId;
+            ProductName = productName;
+            PictureUri = pictureUri;
+        }
+
+        private CatalogItemOrdered()
+        {
+            // required by EF
+        }
+
+        public int CatalogItemId { get; private set; }
+        public string ProductName { get; private set; }
+        public string PictureUri { get; private set; }
+    }
+}
---
+++ src/FunctionApp2/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp2
+{
+    public class MyClass
+    {
+        public string id { get; set; }
+        public string order { get; set; }
+    }
+}
