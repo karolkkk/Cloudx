6bc9b5d3164a1e90599ca5d3b919e5ea7e7e3462
Karolina Krauze <karolina_krauze@epam.com>
2021-11-27 22:22:01 +0100

    delivery order processor

---
+++ src/DeliveryOrderProccesor/Function1.cs
@@ -0,0 +1,48 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace DeliveryOrderProccesor
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(databaseName: "cloudx-final-db", collectionName: "cloudx-final-db-container",
+    ConnectionStringSetting = "CosmosDbConnectionString"
+    )]IAsyncCollector<dynamic> documentsOut, ILogger log)
+        {
+    log.LogInformation("C# HTTP trigger function processed a request.");
+
+    string name = req.Query["name"];
+
+    string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+    dynamic data = JsonConvert.DeserializeObject(requestBody);
+    name = name ?? data?.name;
+
+    if (!string.IsNullOrEmpty(name))
+    {
+        // Add a JSON document to the output container.
+        await documentsOut.AddAsync(new
+        {
+            // create a random ID
+            id = System.Guid.NewGuid().ToString(),
+            name = name
+        });
+    }
+
+    string responseMessage = string.IsNullOrEmpty(name)
+        ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+        : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+    return new OkObjectResult(responseMessage);
+}
+    }
+}
